2f3d9d1f3fe205a31921ed2a0aed4fac707ac4a8
Steve Lilley <SpiesInOrbit@gmail.com>
2024-05-25 13:47:14 -0500

    Add scheduler

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+// modules
 import { DhtSensorModule } from './dht-sensor/dht-sensor.module';
 import { I2cRelayModule } from './i2c-relay/i2c-relay.module';
 
-import { dataBaseConfig } from './database/database.config';
+// utility
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ScheduleModule } from '@nestjs/schedule';
 
+// database
+import { dataBaseConfig } from './database/database.config';
 
 @Module({
-  imports: [DhtSensorModule, I2cRelayModule, SequelizeModule.forRoot(dataBaseConfig)],
+  imports: [
+    DhtSensorModule,
+    I2cRelayModule,
+    SequelizeModule.forRoot(dataBaseConfig),
+    ScheduleModule.forRoot()
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
