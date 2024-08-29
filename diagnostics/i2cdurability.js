7c94ee57c0f82fed57f0566ce1b993540c0290cf
Steve Lilley <SpiesInOrbit@gmail.com>
2024-08-29 17:44:14 -0500

    updated tests

--- diagnostics/i2cdurability.js
+++ diagnostics/i2cdurability.js
@@ -8,8 +8,12 @@ const bus = i2c.openSync(1);
 // Common addresses are 0x20 to 0x27 depending on the A0, A1, A2 pins connections
 const ADDRESS = 0x27;
 
+const delay = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 // Function to send a byte (binary code) to the PCF8574
-function sendByteToPCF8574(byte) {
+async function sendByteToPCF8574(byte) {
   bus.writeByteSync(ADDRESS, 0x00, byte);
   console.log(`Byte ${byte.toString(2).padStart(8, '0')} sent to PCF8574`);
 }
@@ -20,30 +24,41 @@ function readByteFromPCF8574() {
   return byte;
 }
 
-let binAssembly = '';
-const switches = {
-  A: 'off',
-  B: 'off',
-  C: 'off',
-  D: 'off',
-};
+const possible = [
+  '00000000',
+  '10000000',
+  '11000000',
+  '11100000',
+  '11110000',
+  '10100000',
+  '10010000',
+  '00110000',
+  '01110000',
+  '11110000',
+];
 
-binAssembly = switches.A == 'on' ? '0' : '1';
-binAssembly += switches.B == 'on' ? '0' : '1';
-binAssembly += switches.C == 'on' ? '0' : '1';
-binAssembly += switches.D == 'on' ? '0' : '1';
-binAssembly += '0000'; // filler
+async function run(times) {
+  const repeat = times || 100;
+  for (let i = 0; i < repeat; i++) {
+    console.log('Loop: ', i);
+    for (let x = 0; x < possible.length; x++) {
+      await delay(250);
+      const result = sendByteToPCF8574(parseInt(possible[x], 2));
+      console.log('Res', result);
+      console.log(`sending item ${i} for ${possible[x]}`);
+    }
+    console.log(`loop: ${i} complete`);
+  }
+}
 
-// const binaryRep = '00110000'; /// all off
-console.log(`binAss ${binAssembly}`);
-const binaryRep = binAssembly;
-const hexa = parseInt(binaryRep, 2);
+run(100);
+console.log('DONE');
+bus.closeSync();
 // Example to turn on all outputs
 // Sending 0xFF will set all IO pins to HIGH
-console.log('setting:', hexa);
 //sendByteToPCF8574(0xf0);
-sendByteToPCF8574(hexa);
-bus.closeSync();
+
+//sendByteToPCF8574(hexa);
 //const readRes = readByteFromPCF8574();
 // console.log(`Received ${readRes}`);
 // Close the I2C bus after finishing
